a799e20d36df685b62fc9816c0ed9fec817ea362
Olawalessed <51535503+OlawaleSOD@users.noreply.github.com>
2021-09-15 15:19:34 +0100

    Updated landing page image

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ import Header from '../components/fixed/Header'
 export default function Hero() {
     return (
       <div className="container h-screen">
-        <section className="flex bg-gradient-to-r from-purple-900 to-blue-900 relative px-16 py-10 h-full">
+        <section className="flex bg-gradient-to-r from-purple-900 to-blue-900 relative px-16 py-10 h-full items-center overflow-hidden">
           <div className="flex w-1/2">
             <div className="flex flex-col p-10  justify-center">
               <h1 className="text-5xl text-white font-black ">
@@ -18,17 +18,16 @@ export default function Hero() {
                 type="button"
                 className="px-8 py-4 my-5 rounded-md bg-orange text-white w-2/5"
               >
-                <a href="/dashboard">
-                Go to Dashboard
-                </a>
+                <a href="/dashboard">Go to Dashboard</a>
               </button>
             </div>
           </div>
-          <div className="w-1/2">
+          <div className="w-1/2 flex justify-center">
             <Image
-              src="https://www.pexels.com/photo/person-on-truck-s-roof-2449600/"
-              width={512}
-              height={768}
+              src="https://res.cloudinary.com/olawalessed/image/upload/v1631714378/Dashboard%20project/choouse%2B%2B/5-2_oqjqhm.png"
+              width={329}
+              height={341}
+              className="self-center"
             />
           </div>
         </section>
